0db6670d3b1809c441c2665512f3305a144ee4d6
justin-bi-princeton <justinbi@princeton.edu>
2021-12-10 14:57:48 -0500

    12/10 changes

--- test_camera.py
+++ test_camera.py
@@ -3,12 +3,11 @@ import cv2
 
 # This may open your webcam instead of the CrazyFlie camera! If so, try
 # a different small, positive integer, e.g. 1, 2, 3.
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
